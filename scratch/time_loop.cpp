9775f5657e9bdb2425ee46defe26b082dbe04306
tinkha <tinkha@cs.wallawalla.edu>
2020-03-05 02:40:11 +0000

    More Direction Stuff

--- scratch/time_loop.cpp
+++ scratch/time_loop.cpp
@@ -1,30 +1,69 @@
+#include <pthread.h>
 #include <chrono>
+#include <cstdio>
 #include <iostream>
+#include <string>
 #include <thread>
+enum Compass { NORTH, SOUTH, EAST, WEST };
+
 using namespace std;
 using namespace this_thread;  // sleep_for, sleep_until
 using namespace chrono;       // nanoseconds, system_clock, seconds, milliseconds
+char direction = 's';
+string newDirection;
+int stopflag = 0;
+char input = 's';
+void GetDirection() {
+    Compass ChangeDirection;
+    while (true && !stopflag) {
+        system("stty raw");
+        input = getchar();
+        switch (input) {
+            case 'w':
+                if (ChangeDirection != SOUTH) {
+                    ChangeDirection = NORTH;
+                    cout<<ChangeDirection<<endl;
+                }
+                break;
+            case 'a':
+                if (ChangeDirection != EAST) {
+                    ChangeDirection = WEST;
+                    cout<<ChangeDirection<<endl;
+                }
+                break;
+            case 's':
+                if (ChangeDirection != NORTH) {
+                    ChangeDirection = SOUTH;
+                    cout<<ChangeDirection<<endl;
+                }
+                break;
+            case 'd':
+                if (ChangeDirection != WEST) {
+                    ChangeDirection = EAST;
+                    cout<<ChangeDirection<<endl;
+                }
+        }
+        system("stty cooked");
+    }
+}
 
+void CheckSnake() {
+    thread th1(GetDirection);
+    if (input == 'p') {
+        stopflag = 1;
+    }
+    th1.detach();
+}
 int main() {
     char input;
     string direction;
-    for (int i = 0; i < 100; ++i) {
-        cin >> input;
-        switch (input) {
-            case 'w':
-                direction = "North";
-                break;
-            case 's':
-                direction = "South";
-                break;
-            case 'a':
-                direction = "West";
-                break;
-            case 'd':
-                direction = "East";
-        }
-        cout << i << endl;
-        sleep_for(milliseconds(100));
-    }
+    do {
+        CheckSnake();
+        cout << input << endl;
+
+    } while (stopflag == 0);
+    //    th2.join();
+    //    cout<<"Move Snake has joined"<<endl;
+    cout << endl << direction << endl;
     return 0;
 }
