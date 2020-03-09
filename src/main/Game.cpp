9174ece9633df16b595b065adb1ec891094300ee
Ivan Guillen <ivan.guillen@wallawalla.edu>
2020-03-08 21:50:33 -0700

    Test

--- src/main/Game.cpp
+++ src/main/Game.cpp
@@ -9,22 +9,22 @@ using namespace chrono;       // nanoseconds, system_clock, seconds, millisecond
 
 Game::Game() {
     // set map size based on gameDifficulty member variable
-    switch (gameDifficulty) {
-        case L_EASY:
-            mapWidth = 10;
-            mapHeight = 10;
-            break;
+    // switch (gameDifficulty) {
+    //     case L_EASY:
+    //         mapWidth = 10;
+    //         mapHeight = 10;
+    //         break;
 
-        case L_MEDIUM:
-            mapWidth = 15;
-            mapHeight = 15;
-            break;
+    //     case L_MEDIUM:
+    //         mapWidth = 15;
+    //         mapHeight = 15;
+    //         break;
 
-        case L_HARD:
-            mapWidth = 20;
-            mapHeight = 20;
-            break;
-    }
+    //     case L_HARD:
+    //         mapWidth = 20;
+    //         mapHeight = 20;
+    //         break;
+    // }
     // initialize board array with space characters
     // for (int i = 0; i < mapHeight; ++i) {
     //     for (int j = 0; j < mapWidth; ++j) {
