d6c783f3ac9f447eef57828e74b78efbc2fb6a26
Luke Irvine <lukeirvinepiano@gmail.com>
2020-03-16 14:47:40 -0700

    formatting

--- src/main/Fruit.cpp
+++ src/main/Fruit.cpp
@@ -1,8 +1,8 @@
 #include "Fruit.h"
+#include <cmath>
 #include <ctime>
 #include <iostream>
 #include <string>
-#include <cmath>
 #include "Game.h"
 #include "Snake.h"
 using namespace std;
@@ -14,7 +14,8 @@ void Fruit::setPosition(int mapHeight, int mapWidth, Point snakeHead, int board[
     do {
         fruitX = rand() % (mapWidth) + 1;   // Generates food at a random X position
         fruitY = rand() % (mapHeight) + 1;  // Generates food at a random Y position
-    } while (board[fruitY][fruitX] > 0 || abs(snakeHead.x - fruitX) < 2 || abs(snakeHead.y - fruitY) < 2);  // this will be to check if location is free or not later on
+    } while (board[fruitY][fruitX] > 0 || abs(snakeHead.x - fruitX) < 2 ||
+             abs(snakeHead.y - fruitY) < 2);  // this will be to check if location is free or not later on
     fruitPosition.x = fruitX;
     fruitPosition.y = fruitY;
     board[fruitPosition.y][fruitPosition.x] = -3;
@@ -23,4 +24,3 @@ void Fruit::setPosition(int mapHeight, int mapWidth, Point snakeHead, int board[
 Point Fruit::getPosition() const {
     return fruitPosition;
 }
-
